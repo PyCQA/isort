516c18a74f1b606b03ab07091cb0004e75c0a49b
Timothy Crosley <timothy.crosley@gmail.com>
2013-09-04 21:24:14 -0400

    Fix kate plugin

--- kate_plugin.py
+++ kate_plugin.py
@@ -21,10 +21,10 @@
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 """
 
+from isort import SortImports
 from PyKDE4.ktexteditor import KTextEditor
 
 import kate
-from isort import SortImports
 
 
 @kate.action(text="Sort Imports", shortcut="Ctrl+[", menu="Python")
