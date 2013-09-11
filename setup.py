bf1bd90e5b0cc14769319aabfbe7c5dc7666ed6d
Timothy Crosley <timothy.crosley@gmail.com>
2013-09-11 17:44:22 -0400

    Release 1.2.5: switch to setup tools, fix ignore file bug

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup
 
 setup(name='isort',
-      version='1.2.4',
+      version='1.2.5',
       description='A Python utility / library to sort Python imports.',
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='https://github.com/timothycrosley/isort',
       download_url='https://github.com/timothycrosley/isort/blob/master'
-                   '/dist/isort-1.2.4.tar.gz?raw=true',
+                   '/dist/isort-1.2.5.tar.gz?raw=true',
       license="MIT",
       scripts=['scripts/isort'],
       packages=['isort'],
