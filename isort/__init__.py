3331c40a324009b2d25bc46284bcf0bd241ac24d
Timothy Crosley <timothy.crosley@gmail.com>
2013-10-07 18:53:39 -0400

    Bump release to 2.0.1

--- isort/__init__.py
+++ isort/__init__.py
@@ -25,4 +25,4 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from . import settings
 from .isort import SortImports
 
-__version__ = "2.0.0"
+__version__ = "2.0.1"
