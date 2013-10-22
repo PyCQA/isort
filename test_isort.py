96ce1ec793e78a16a2a41c093988d0ea27f82923
Timothy Crosley <timothy.crosley@gmail.com>
2013-10-21 22:12:51 -0400

    isort

--- test_isort.py
+++ test_isort.py
@@ -21,8 +21,9 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from isort.isort import SortImports
 from isort.settings import WrapModes
+from isort.isort import SortImports
+
 
 REALLY_LONG_IMPORT = ("from third_party import lib1, lib2, lib3, lib4, lib5, lib6, lib7, lib8, lib9, lib10, lib11,"
                       "lib12, lib13, lib14, lib15, lib16, lib17, lib18, lib20, lib21, lib22")
