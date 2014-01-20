2ed4749136798b233594d1ba0f3748884901324b
Timothy Crosley <timothy.crosley@gmail.com>
2014-01-20 06:44:31 -0500

    Bump version to 3.1.2

--- isort/__init__.py
+++ isort/__init__.py
@@ -25,4 +25,4 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from . import settings
 from .isort import SECTION_NAMES, SECTIONS, SortImports
 
-__version__ = "3.1.1"
+__version__ = "3.1.2"
