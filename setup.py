87ecdc820af84498db4479c8f7174f573c6a36f4
Timothy Crosley <timothy.crosley@gmail.com>
2014-01-23 23:05:48 -0500

    Bump version to 3.2.0

--- setup.py
+++ setup.py
@@ -42,13 +42,13 @@ except (IOError, ImportError, OSError, RuntimeError):
    readme = ''
 
 setup(name='isort',
-      version='3.1.2',
+      version='3.2.0',
       description='A Python utility / library to sort Python imports.',
       long_description=readme,
       author='Timothy Crosley',
       author_email='timothy.crosley@gmail.com',
       url='https://github.com/timothycrosley/isort',
-      download_url='https://github.com/timothycrosley/isort/archive/3.1.2.tar.gz',
+      download_url='https://github.com/timothycrosley/isort/archive/3.2.0.tar.gz',
       license="MIT",
       entry_points={
         'console_scripts': [
