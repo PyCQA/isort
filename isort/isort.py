c26a594e5e375378c15ec2e668f40a01c35cb91c
Timothy Crosley <timothy.crosley@gmail.com>
2014-01-23 21:47:10 -0500

    Use current working directory as fallback

--- isort/isort.py
+++ isort/isort.py
@@ -50,7 +50,7 @@ class SortImports(object):
 
     def __init__(self, file_path=None, file_contents=None, write_to_stdout=False, check=False,
                  show_diff=False, settings_path=None, **setting_overrides):
-        self.config = settings.from_path(settings_path or os.path.dirname(os.path.abspath(file_path)))
+        self.config = settings.from_path(settings_path or os.path.dirname(os.path.abspath(file_path or os.getcwd())))
         if setting_overrides:
             self.config = settings.default.copy()
             self.config.update(setting_overrides)
