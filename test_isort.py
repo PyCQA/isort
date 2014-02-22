5bd23e3f93f1c22c4a378d1c0c83aa5b50700902
Timothy Crosley <timothy.crosley@gmail.com>
2014-02-21 20:17:58 -0500

    Add additional test

--- test_isort.py
+++ test_isort.py
@@ -723,16 +723,24 @@ def test_keep_comments():
     assert SortImports(file_contents=test_input, combine_as_imports=True).output == test_input
 
     # More complicated case
-    test_input_wll_merge = ("from a import b # My Comment1\n"
+    test_input = ("from a import b # My Comment1\n"
                             "from a import c # My Comment2\n")
-    assert SortImports(file_contents=test_input_wll_merge, combine_as_imports=True).output == \
+    assert SortImports(file_contents=test_input, combine_as_imports=True).output == \
                       ("from a import b, c # My Comment1; My Comment2\n")
 
     # Test case where imports comments make imports extend pass the line length
-    # More complicated case
-    test_input_wll_merge = ("from a import b # My Comment1\n"
+    test_input = ("from a import b # My Comment1\n"
                             "from a import c # My Comment2\n"
                             "from a import d\n")
-    assert SortImports(file_contents=test_input_wll_merge, combine_as_imports=True, line_length=45).output == \
+    assert SortImports(file_contents=test_input, combine_as_imports=True, line_length=45).output == \
                       ("from a import (b, # My Comment1; My Comment2\n"
+                       "               c, d)\n")
+
+    # Test case where imports with comments will be beyond line length limit
+    test_input = ("from a import b # My Comment1\n"
+                            "from a import c # My Comment2 is really really really really long\n"
+                            "from a import d\n")
+
+    assert SortImports(file_contents=test_input, combine_as_imports=True, line_length=45).output == \
+                      ("from a import (b, # My Comment1; My Comment2 is really really really really long\n"
                        "               c, d)\n")
