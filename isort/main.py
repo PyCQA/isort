98e1394ce51379f7fc86f169cbc374030933b170
Timothy Crosley <timothy.crosley@gmail.com>
2014-04-16 23:28:54 -0400

    Add force add capability to handle case where users want to force adds to take place even for currently empty files

--- isort/main.py
+++ isort/main.py
@@ -64,6 +64,8 @@ def main():
     parser.add_argument('-a', '--add_import', dest='add_imports', action='append',
                         help='Adds the specified import line to all files, '
                              'automatically determining correct placement.')
+    parser.add_argument('-af', '--force_adds', dest='force_adds', action='append',
+                        help='Forces import adds even if the original file is empty.')
     parser.add_argument('-r', '--remove_import', dest='remove_imports', action='append',
                         help='Removes the specified import from all files.')
     parser.add_argument('-ls', '--length_sort', help='Sort imports by their string length.',
