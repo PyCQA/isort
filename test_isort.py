08bc1b0c92a2cd45baa16aa1884bf72ef7059127
Timothy Crosley <timothy.crosley@gmail.com>
2014-12-21 13:52:01 -0500

    Add test for desired output format

--- test_isort.py
+++ test_isort.py
@@ -1047,3 +1047,10 @@ def test_tab_character_in_import():
     test_input = ("from __future__ import print_function\n"
                   "from __future__ import\tprint_function\n")
     assert SortImports(file_contents=test_input).output == "from __future__ import print_function\n"
+
+
+def test_split_position():
+    test_input = ("from p24.shared.exceptions.master.host_state_flag_unchanged import HostStateUnchangedException\n")
+    assert SortImports(file_contents=test_input, line_length=80).output == \
+                                            ("from p24.shared.exceptions.master.host_state_flag_unchanged import \\\n"
+                                            "   HostStateUnchangedException\n")
