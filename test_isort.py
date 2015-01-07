b44c815732b322859819ff9102d5a76f2e6df9e9
Timothy Crosley <timothy.crosley@gmail.com>
2015-01-07 14:23:44 -0500

    Fix copyright notice

--- test_isort.py
+++ test_isort.py
@@ -3,6 +3,8 @@
 Tests all major functionality of the isort library
 Should be ran using py.test by simply running by.test in the isort project directory
 
+Copyright (C) 2013  Timothy Edmund Crosley
+
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation
 the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
