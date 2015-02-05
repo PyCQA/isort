db059715209c7dda570bb20ea4783a3c80b8df0f
Timothy Crosley <timothy.crosley@gmail.com>
2015-02-05 16:19:55 -0500

    Pass wrap_length setting from console application

--- isort/main.py
+++ isort/main.py
@@ -107,7 +107,9 @@ def main():
     parser.add_argument('-sp', '--settings-path',  dest="settings_path",
                         help='Explicitly set the settings path instead of auto determining based on file location.')
     parser.add_argument('-ff', '--from-first', dest='from_first',
-                        help="Switches the typical ordering preference, showing from imports first then straight ones")
+                        help="Switches the typical ordering preference, showing from imports first then straight ones.")
+    parser.add_argument('-wl', '--wrap-length', dest='wrap_length',
+                        help="Specifies how long lines that are wrapped should be, if not set line_length is used.")
 
     arguments = dict((key, value) for (key, value) in itemsview(vars(parser.parse_args())) if value)
     file_names = arguments.pop('files', [])
