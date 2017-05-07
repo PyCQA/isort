8559b531cdc48f7b191cd05e0a0f02c48fef5574
Jon Dufresne <jon.dufresne@gmail.com>
2017-05-07 05:12:35 -0700

    Drop support for Python 2.6
    I would humbly like to suggest isort drops support for Python 2.6.
    
    The last release of Python 2.6 was 2013-10-29, over 3 years ago. It is
    no longer receiving security fixes.
    
    https://www.python.org/dev/peps/pep-0361/
    
    The pip project itself has recently dropped support for 2.6. Their
    numbers estimate that Python 2.6 accounts for ~2% of their downloads.
    See:
    
    https://github.com/pypa/pip/pull/4343
    
    For projects that still use Python 2.6, they can continue to pip install
    an older version of isort.
    
    I've tried my best to remove as much 2.6 specific code as I can,
    including the 'Programming Language :: Python :: 2.6' trove classifier
    from setup.py. I've also removed Travis CI testing, which should result
    in faster testing and fewer wasted resources.
    
    Code changed:
    
    - Removed Python2.6 from documentation
    - Removed Python2.6 from testing configuration
    - Use set literals
    - Use dict comprehension
    - Pass generator expressions to builtin functions instead of lists
    - Use builtin OrderedDict
    - Removed deprecated U open mode; replace with io.open() which support
      universal newlines
    - Removed Python2.6 workarounds throughout the code
    
    Thanks for considering.

--- isort/pie_slice.py
+++ isort/pie_slice.py
@@ -22,9 +22,9 @@ OTHER DEALINGS IN THE SOFTWARE.
 from __future__ import absolute_import
 
 import abc
+import collections
 import functools
 import sys
-import collections
 from numbers import Integral
 
 __version__ = "1.1.0"
@@ -47,7 +47,7 @@ native_object = object
 
 common = ['native_dict', 'native_round', 'native_filter', 'native_map', 'native_range', 'native_str', 'native_chr',
           'native_input', 'PY2', 'PY3', 'u', 'itemsview', 'valuesview', 'keysview', 'execute', 'integer_types',
-          'native_next', 'native_object', 'with_metaclass', 'OrderedDict', 'lru_cache']
+          'native_next', 'native_object', 'with_metaclass', 'lru_cache']
 
 
 def with_metaclass(meta, *bases):
@@ -85,32 +85,12 @@ def unmodified_isinstance(*bases):
 
     """
     class UnmodifiedIsInstance(type):
-        if sys.version_info[0] == 2 and sys.version_info[1] <= 6:
+        @classmethod
+        def __instancecheck__(cls, instance):
+            if cls.__name__ in (str(base.__name__) for base in bases):
+                return isinstance(instance, bases)
 
-            @classmethod
-            def __instancecheck__(cls, instance):
-                if cls.__name__ in (str(base.__name__) for base in bases):
-                    return isinstance(instance, bases)
-
-                subclass = getattr(instance, '__class__', None)
-                subtype = type(instance)
-                instance_type = getattr(abc, '_InstanceType', None)
-                if not instance_type:
-                    class test_object:
-                        pass
-                    instance_type = type(test_object)
-                if subtype is instance_type:
-                    subtype = subclass
-                if subtype is subclass or subclass is None:
-                    return cls.__subclasscheck__(subtype)
-                return (cls.__subclasscheck__(subclass) or cls.__subclasscheck__(subtype))
-        else:
-            @classmethod
-            def __instancecheck__(cls, instance):
-                if cls.__name__ in (str(base.__name__) for base in bases):
-                    return isinstance(instance, bases)
-
-                return type.__instancecheck__(cls, instance)
+            return type.__instancecheck__(cls, instance)
 
     return with_metaclass(UnmodifiedIsInstance, *bases)
 
@@ -280,166 +260,16 @@ else:
             dct['__str__'] = lambda self: self.__unicode__().encode('utf-8')
             return type.__new__(cls, name, bases, dct)
 
-        if sys.version_info[1] <= 6:
-            def __instancecheck__(cls, instance):
-                if cls.__name__ == "object":
-                    return isinstance(instance, native_object)
-
-                subclass = getattr(instance, '__class__', None)
-                subtype = type(instance)
-                instance_type = getattr(abc, '_InstanceType', None)
-                if not instance_type:
-                    class test_object:
-                        pass
-                    instance_type = type(test_object)
-                if subtype is instance_type:
-                    subtype = subclass
-                if subtype is subclass or subclass is None:
-                    return cls.__subclasscheck__(subtype)
-                return (cls.__subclasscheck__(subclass) or cls.__subclasscheck__(subtype))
-        else:
-            def __instancecheck__(cls, instance):
-                if cls.__name__ == "object":
-                    return isinstance(instance, native_object)
-                return type.__instancecheck__(cls, instance)
+        def __instancecheck__(cls, instance):
+            if cls.__name__ == "object":
+                return isinstance(instance, native_object)
+            return type.__instancecheck__(cls, instance)
 
     class object(with_metaclass(FixStr, object)):
         pass
 
     __all__ = common + ['round', 'dict', 'apply', 'cmp', 'coerce', 'execfile', 'raw_input', 'unpacks', 'str', 'chr',
                         'input', 'range', 'filter', 'map', 'zip', 'object']
-
-if sys.version_info[0] == 2 and sys.version_info[1] < 7:
-    # OrderedDict
-    # Copyright (c) 2009 Raymond Hettinger
-    #
-    # Permission is hereby granted, free of charge, to any person
-    # obtaining a copy of this software and associated documentation files
-    # (the "Software"), to deal in the Software without restriction,
-    # including without limitation the rights to use, copy, modify, merge,
-    # publish, distribute, sublicense, and/or sell copies of the Software,
-    # and to permit persons to whom the Software is furnished to do so,
-    # subject to the following conditions:
-    #
-    #     The above copyright notice and this permission notice shall be
-    #     included in all copies or substantial portions of the Software.
-    #
-    #     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    #     EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-    #     OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-    #     NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-    #     HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-    #     WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-    #     FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-    #     OTHER DEALINGS IN THE SOFTWARE.
-
-    from UserDict import DictMixin
-
-    class OrderedDict(dict, DictMixin):
-
-        def __init__(self, *args, **kwds):
-            if len(args) > 1:
-                raise TypeError('expected at most 1 arguments, got %d' % len(args))
-            try:
-                self.__end
-            except AttributeError:
-                self.clear()
-            self.update(*args, **kwds)
-
-        def clear(self):
-            self.__end = end = []
-            end += [None, end, end]         # sentinel node for doubly linked list
-            self.__map = {}                 # key --> [key, prev, next]
-            dict.clear(self)
-
-        def __setitem__(self, key, value):
-            if key not in self:
-                end = self.__end
-                curr = end[1]
-                curr[2] = end[1] = self.__map[key] = [key, curr, end]
-            dict.__setitem__(self, key, value)
-
-        def __delitem__(self, key):
-            dict.__delitem__(self, key)
-            key, prev, next = self.__map.pop(key)
-            prev[2] = next
-            next[1] = prev
-
-        def __iter__(self):
-            end = self.__end
-            curr = end[2]
-            while curr is not end:
-                yield curr[0]
-                curr = curr[2]
-
-        def __reversed__(self):
-            end = self.__end
-            curr = end[1]
-            while curr is not end:
-                yield curr[0]
-                curr = curr[1]
-
-        def popitem(self, last=True):
-            if not self:
-                raise KeyError('dictionary is empty')
-            if last:
-                key = reversed(self).next()
-            else:
-                key = iter(self).next()
-            value = self.pop(key)
-            return key, value
-
-        def __reduce__(self):
-            items = [[k, self[k]] for k in self]
-            tmp = self.__map, self.__end
-            del self.__map, self.__end
-            inst_dict = vars(self).copy()
-            self.__map, self.__end = tmp
-            if inst_dict:
-                return (self.__class__, (items,), inst_dict)
-            return self.__class__, (items,)
-
-        def keys(self):
-            return list(self)
-
-        setdefault = DictMixin.setdefault
-        update = DictMixin.update
-        pop = DictMixin.pop
-        values = DictMixin.values
-        items = DictMixin.items
-        iterkeys = DictMixin.iterkeys
-        itervalues = DictMixin.itervalues
-        iteritems = DictMixin.iteritems
-
-        def __repr__(self):
-            if not self:
-                return '%s()' % (self.__class__.__name__,)
-            return '%s(%r)' % (self.__class__.__name__, self.items())
-
-        def copy(self):
-            return self.__class__(self)
-
-        @classmethod
-        def fromkeys(cls, iterable, value=None):
-            d = cls()
-            for key in iterable:
-                d[key] = value
-            return d
-
-        def __eq__(self, other):
-            if isinstance(other, OrderedDict):
-                if len(self) != len(other):
-                    return False
-                for p, q in zip(self.items(), other.items()):
-                    if p != q:
-                        return False
-                return True
-            return dict.__eq__(self, other)
-
-        def __ne__(self, other):
-            return not self == other
-else:
-    from collections import OrderedDict
 
 
 if sys.version_info < (3, 2):
@@ -487,7 +317,7 @@ if sys.version_info < (3, 2):
                     misses[0] += 1
                     return result
             else:
-                CACHE = OrderedDict()
+                CACHE = collections.OrderedDict()
 
                 @wraps(user_function)
                 def wrapper(*args, **kwds):
