bc3cda5928c3b6d03639a90b6bd41a2c964f6af6
Dylan Katz <git@dylankatz.com>
2017-10-12 11:36:57 -0600

    Properly fixed line endings and added option to command

--- test_isort.py
+++ test_isort.py
@@ -2199,3 +2199,16 @@ def test_ensure_as_imports_sort_correctly_within_from_imports_issue_590():
     test_input = ('from os import defpath\n'
                   'from os import pathsep as separator\n')
     assert SortImports(file_contents=test_input, force_single_line=True).output == test_input
+
+
+def test_ensure_line_endings_are_preserved_issue_493():
+    """Test to ensure line endings are not converted"""
+    test_input = ('from os import defpath\r\n'
+                  'from os import pathsep as separator\r\n')
+    assert SortImports(file_contents=test_input).output == test_input
+    test_input = ('from os import defpath\r'
+                  'from os import pathsep as separator\r')
+    assert SortImports(file_contents=test_input).output == test_input
+    test_input = ('from os import defpath\n'
+                  'from os import pathsep as separator\n')
+    assert SortImports(file_contents=test_input).output == test_input
