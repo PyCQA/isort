11e19462d0ca24469ab3fe0bcf3e60c1960b69d5
Sam Lai <sam@citymapper.com>
2018-05-27 17:51:47 +0100

    Fix lint issue

--- isort/__main__.py
+++ isort/__main__.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import
 from .pie_slice import apply_changes_to_python_environment
 apply_changes_to_python_environment()
 
-from isort.main import main
+from isort.main import main  # noqa: E402
 
 main()
