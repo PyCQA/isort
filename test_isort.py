da544deb4f3ffd2411013cbfa75761089664dcda
Jeremiah Paige <jeremiahcpaige@gmail.com>
2019-02-25 13:41:24 -0800

    Keep trailing comma even for a single from import. Issue 831

--- test_isort.py
+++ test_isort.py
@@ -1304,7 +1304,7 @@ def test_include_trailing_comma():
     ).output
     assert test_output_wrap_single_import_vertical_indent == (
         "from third_party import (\n"
-        "    lib1\n"
+        "    lib1,\n"
         ")\n"
     )
 
