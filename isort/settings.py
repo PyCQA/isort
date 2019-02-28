f8c3ea48a5c96265616639e7094f92c708a6da98
Timothy Crosley <timothy.crosley@gmail.com>
2019-02-28 00:39:13 -0800

    Convert path into OS specific path

--- isort/settings.py
+++ isort/settings.py
@@ -336,7 +336,8 @@ def should_skip(filename, config, path='/'):
         if fnmatch.fnmatch(filename, glob):
             return True
 
-    if not (os.path.isfile(normalized_path) or os.path.isdir(normalized_path) or os.path.islink(normalized_path)):
+    os_specific_path = os.path.join(posixpath.split(normalized_path))
+    if not (os.path.isfile(os_specific_path) or os.path.isdir(os_specific_path) or os.path.islink(os_specific_path)):
         return True
 
     return False
