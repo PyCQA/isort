4a520029838440645a8fdc0387fb8206249f4f80
Anthony Sottile <asottile@umich.edu>
2019-03-01 08:27:18 -0800

    Fix whitespace in warning message

--- isort/settings.py
+++ isort/settings.py
@@ -288,7 +288,7 @@ def _get_config_data(file_path, sections):
                     settings.update(config_section)
             else:
                 warnings.warn(
-                    "Found %s but toml package is not installed. To configure"
+                    "Found %s but toml package is not installed. To configure "
                     "isort with %s, install with 'isort[pyproject]'." % (file_path, file_path)
                 )
         else:
