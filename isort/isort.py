28c395cca63858853da52235a70fb49d493305bb
Timothy Crosley <timothy.crosley@gmail.com>
2019-06-25 17:04:59 -0700

    Fix wrap mode check

--- isort/isort.py
+++ isort/isort.py
@@ -386,8 +386,8 @@ class SortImports(object):
                             output = "{0}{1}({2}{3}{4}{5})".format(
                                 line, splitter, self.line_separator, cont_line,
                                 "," if self.config['include_trailing_comma'] else "",
-                                self.line_separator if wrap_mode in (WrapModes.VERTICAL_HANGING_INDENT,
-                                                                     WrapModes.VERTICAL_GRID_GROUPED)
+                                self.line_separator if wrap_mode in (settings.WrapModes.VERTICAL_HANGING_INDENT,
+                                                                     settings.WrapModes.VERTICAL_GRID_GROUPED)
                                 else "")
                         lines = output.split(self.line_separator)
                         if self.config['comment_prefix'] in lines[-1] and lines[-1].endswith(')'):
