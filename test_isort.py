8b63c747736d388788c08cc922e150ed94aabac8
Timothy Crosley <timothy.crosley@gmail.com>
2019-06-25 17:53:28 -0700

    Update to stay within line length limit

--- test_isort.py
+++ test_isort.py
@@ -2207,9 +2207,12 @@ def test_long_alias_using_paren_issue_957():
     out = SortImports(file_contents=test_input, line_length=50, use_parentheses=True, multi_line_output=WrapModes.VERTICAL_GRID_GROUPED, check=True).output
     assert out == expected_output
 
-    test_input = ('from deep.deep.deep.deep.deep.deep.deep.deep.deep.package import very_very_very_very_very_very_very_very_very_very_long_module as very_very_very_very_very_very_very_very_very_very_long_alias\n')
+    test_input = ('from deep.deep.deep.deep.deep.deep.deep.deep.deep.package import very_very_very'
+                  '_very_very_very_very_very_very_very_long_module as very_very_very_very_very'
+                  '_very_very_very_very_very_long_alias\n')
     expected_output = ('from deep.deep.deep.deep.deep.deep.deep.deep.deep.package import (\n'
-                       '    very_very_very_very_very_very_very_very_very_very_long_module as very_very_very_very_very_very_very_very_very_very_long_alias\n'
+                       '    very_very_very_very_very_very_very_very_very_very_long_module as very'
+                       '_very_very_very_very_very_very_very_very_very_long_alias\n'
                        ')\n')
     out = SortImports(file_contents=test_input, line_length=50, use_parentheses=True, multi_line_output=WrapModes.VERTICAL_GRID_GROUPED, check=True).output
     assert out == expected_output
