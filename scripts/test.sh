2be31385349bb92cb1d0cbc30ee175ac00ec3db2
Timothy Crosley <timothy.crosley@gmail.com>
2019-10-13 23:47:38 -0700

    Update to latest template

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 ./scripts/lint.sh
-poetry run pytest tests/ -s --cov=isort/ --cov=tests/ --cov-report=term-missing ${@} --cov-report html
+poetry run pytest tests/ -s --cov=isort/ --cov=tests/ --cov-report=term-missing ${@-} --cov-report html
