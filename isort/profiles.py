5fccf078c654b69344ded47c2f4c7abddbd52c4d
Timothy Crosley <timothy.crosley@gmail.com>
2019-11-09 00:27:51 -0800

    Add initial profiles

---
+++ isort/profiles.py
@@ -0,0 +1,55 @@
+"""Common profiles are defined here to be easily used within a project using --profile {name}"""
+
+black = {
+    "multi_line_output": 3,
+    "include_trailing_comma": True,
+    "force_grid_wrap": 0,
+    "use_parentheses": True,
+}
+django = {
+    "combine_as_imports": True,
+    "include_trailing_comma": True,
+    "multi_line_output": 5,
+    "line_length": 79,
+}
+pycharm = {"multi_line_output": 3, "force_grid_wrap": 2}
+google = {"force_single_line": True, "force_sort_within_sections": True, "lexicographical": True}
+open_stack = {
+    "force_single_line": True,
+    "force_sort_within_sections": True,
+    "lexicographical": True,
+}
+plone = {
+    "force_alphabetical_sort": True,
+    "force_single_line": True,
+    "ines_after_imports": 2,
+    "line_length": 200,
+}
+attrs = {
+    "atomic": True,
+    "force_grid_wrap": 0,
+    "include_trailing_comma": True,
+    "lines_after_imports": 2,
+    "lines_between_types": 1,
+    "multi_line_output": 3,
+    "not_skip": "__init__.py",
+    "use_parentheses": True,
+}
+hug = {
+    "multi_line_output": 3,
+    "include_trailing_comma": True,
+    "force_grid_wrap": 0,
+    "use_parentheses": True,
+    "line_length": 100,
+}
+
+profiles = {
+    "black": black,
+    "django": django,
+    "pycharm": pycharm,
+    "google": google,
+    "open_stack": open_stack,
+    "plone": plone,
+    "attrs": attrs,
+    "hug": hug,
+}
