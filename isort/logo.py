10dcee3944d29d3e99e992326f9dd1260fb97260
Timothy Crosley <timothy.crosley@gmail.com>
2019-11-10 01:32:04 -0800

    Simplified CLI logo to match isort 5.0 logo

--- isort/logo.py
+++ isort/logo.py
@@ -1,26 +1,15 @@
 from ._version import __version__
 
 ASCII_ART = rf"""
-/#######################################################################\
+                    _                 _
+                   (_) ___  ___  _ __| |_
+                  / / / _/ / _ \/ '__  _/
+                 / /  \__ \/\_\/| |  | |_
+                /_/   \___/\___/\_/   \_/
 
-     `sMMy`
-     .yyyy-                                                      `
-    ##soos##                                                    ./o.
-          `     ``..-..`         ``...`.``         `   ```` ``-ssso```
-     .s:-y-   .+osssssso/.     ./ossss+:so+:`    :+o-`/osso:+sssssssso/
-     .s::y-   osss+.``.``     -ssss+-.`-ossso`   ssssso/::..::+ssss:::.
-     .s::y-   /ssss+//:-.`   `ssss+     `ssss+   sssso`       :ssss`
-     .s::y-   `-/+oossssso/  `ssss/      sssso   ssss/        :ssss`
-     .y-/y-       ````:ssss`  ossso.    :ssss:   ssss/        :ssss.
-     `/so:`    `-//::/osss+   `+ssss+-/ossso:    /sso-        `osssso/.
-       \/      `-/oooo++/-      .:/++:/++/-`      ..           `://++/.
+ isort your Python imports for you so you don't have to.
 
-
-         isort your Python imports for you so you don't have to
-
-                            VERSION {__version__}
-
-\########################################################################/
+                    VERSION {__version__}
 """
 
 __doc__ = f"""
