1bb36805ac974d54e554d4c5f4b10e01c79b5b0c
Timothy Crosley <timothy.crosley@gmail.com>
2020-01-21 23:06:24 -0800

    Remove unused functions

--- isort/utils.py
+++ isort/utils.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from contextlib import contextmanager
-from typing import Any, Container, Iterable, Iterator, List
+from typing import Iterator
 
 
 def exists_case_sensitive(path: str) -> bool:
@@ -27,24 +27,3 @@ def chdir(path: str) -> Iterator[None]:
         yield
     finally:
         os.chdir(curdir)
-
-
-def union(a: Iterable[Any], b: Iterable[Any]) -> List[Any]:
-    """Return a list of items that are in `a` or `b`"""
-    unioned: List[Any] = []
-    for item in a:
-        if item not in unioned:
-            unioned.append(item)
-    for item in b:
-        if item not in unioned:
-            unioned.append(item)
-    return unioned
-
-
-def difference(a: Iterable[Any], b: Container[Any]) -> List[Any]:
-    """Return a list of items from `a` that are not in `b`."""
-    differences = []
-    for item in a:
-        if item not in b:
-            differences.append(item)
-    return differences
