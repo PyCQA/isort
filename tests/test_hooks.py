66b5363287a5a071193480278a670df34780d846
Timothy Crosley <timothy.crosley@gmail.com>
2020-01-22 23:40:50 -0800

    Add testing for hooks

---
+++ tests/test_hooks.py
@@ -0,0 +1,25 @@
+from io import BytesIO
+from unittest.mock import MagicMock, patch
+
+from isort import hooks
+
+
+def test_git_hook():
+    """Simple smoke level testing of git hooks"""
+
+    # Ensure correct subprocess command is called
+    with patch("subprocess.run", MagicMock()) as run_mock:
+        hooks.git_hook()
+        assert run_mock.called_with(
+            ["git", "diff-index", "--cached", "--name-only", "--diff-filter=ACMRTUXB HEAD"]
+        )
+
+    # Test with incorrectly sorted file returned from git
+    with patch("isort.hooks.get_lines", MagicMock(return_value=["isort/isort.py"])) as run_mock:
+
+        class FakeProecssResponse(object):
+            stdout = b"import b\nimport a"
+
+        with patch("subprocess.run", MagicMock(return_value=FakeProecssResponse())) as run_mock:
+            with patch("isort.hooks.SortImports", MagicMock()):
+                hooks.git_hook(modify=True)
