b86b97353fc47340c12336623646dba180da3afd
Timothy Crosley <timothy.crosley@gmail.com>
2020-01-26 23:44:15 -0800

    Add src and test dir fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,17 @@
 """isort test wide fixtures and configuration"""
+import os
+
+import pytest
+
+TEST_DIR = os.path.dirname(os.path.abspath(__file__))
+SRC_DIR = os.path.join(TEST_DIR, "../isort/")
+
+
+@pytest.fixture
+def test_dir():
+    return TEST_DIR
+
+
+@pytest.fixture
+def src_dir():
+    return SRC_DIR
