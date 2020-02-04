6f9d02510ad861bf8ae5ad8f1ae335a4e565756d
Timothy Crosley <timothy.crosley@gmail.com>
2020-02-03 23:38:03 -0800

    Add initial unit tests for io module

---
+++ tests/test_io.py
@@ -0,0 +1,23 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from isort import io
+
+
+class TestFile:
+    def test_read(self, tmpdir):
+        test_file_content = """# -*- encoding: ascii -*-
+
+import ☺
+"""
+        test_file = tmpdir.join("file.py")
+        test_file.write(test_file_content)
+
+        # able to read file even with incorrect encoding, if it is UTF-8 compatible
+        assert io.File.read(test_file).contents == test_file_content
+
+        # unless the locale is also ASCII
+        with pytest.raises(io.UnableToDetermineEncoding):
+            with patch("locale.getpreferredencoding", lambda value: "ascii"):
+                io.File.read(test_file).contents
