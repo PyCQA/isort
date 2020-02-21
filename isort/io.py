490e135840908efd06399430c053643ac074df91
Timothy Crosley <timothy.crosley@gmail.com>
2020-02-20 22:54:56 -0800

    Fix open to match stdlibs exactly, with newline modification

--- isort/io.py
+++ isort/io.py
@@ -41,6 +41,7 @@ class File(NamedTuple):
             encoding, lines = tokenize.detect_encoding(buffer.readline)
             buffer.seek(0)
             text = TextIOWrapper(buffer, encoding, line_buffering=True, newline="")
+            text.mode = "r"  # type: ignore
             return text
         except Exception:
             buffer.close()
