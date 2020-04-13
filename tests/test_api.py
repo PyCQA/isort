9a7d11e577e6576ce0eba5708e9cf8588f368fdf
Timothy Crosley <timothy.crosley@gmail.com>
2020-04-12 23:19:23 -0700

    add test for showing diff and writing to stdout when sorting file

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,19 +4,24 @@ import pytest
 from isort import api, exceptions
 
 
-def test_sort_file_invalid_syntax(tmpdir) -> None:
-    """Test to ensure file encoding is respected"""
+def test_sort_file(tmpdir) -> None:
     tmp_file = tmpdir.join(f"test_bad_syntax.py")
     tmp_file.write_text("""print('mismathing quotes")""", "utf8")
     with pytest.warns(UserWarning):
         api.sort_file(tmp_file, atomic=True)
+    with pytest.warns(UserWarning):
+        api.sort_file(tmp_file, atomic=True, write_to_stdout=True)
+
+    imperfect = tmpdir.join(f"test_needs_changes.py")
+    imperfect.write_text("import b\nimport a\n", "utf8")
+    api.sort_file(imperfect, write_to_stdout=True, show_diff=True)
 
 
 def test_check_file(tmpdir) -> None:
     perfect = tmpdir.join(f"test_no_changes.py")
     perfect.write_text("import a\nimport b\n", "utf8")
     assert api.check_file(perfect, show_diff=True)
-    
+
     imperfect = tmpdir.join(f"test_needs_changes.py")
     imperfect.write_text("import b\nimport a\n", "utf8")
     assert not api.check_file(imperfect, show_diff=True)
