62ffabb43f550d4a860bff09016332343983b191
Timothy Crosley <timothy.crosley@gmail.com>
2020-05-02 02:04:24 -0700

    Full test coverage for io module

--- tests/test_io.py
+++ tests/test_io.py
@@ -11,7 +11,31 @@ class TestFile:
     def test_read(self, tmpdir):
         test_file_content = """# -*- encoding: ascii -*-
 
-import ☺
+import Ὡ
 """
         test_file = tmpdir.join("file.py")
         test_file.write(test_file_content)
+        with pytest.raises(Exception):
+            with io.read_file(str(test_file)) as file_handler:
+                file_handler.stream.read()
+
+    def test_from_content(self, tmpdir):
+        test_file = tmpdir.join("file.py")
+        test_file.write_text("import os", "utf8")
+        assert io.File.from_contents("import os", filename=str(test_file))
+
+    def test_open(self, tmpdir):
+        with pytest.raises(Exception):
+            io.File._open("THISCANTBEAREALFILEὩὩὩὩὩὩὩὩὩὩὩὩ.ὩὩὩὩὩ")
+
+        def raise_arbitrary_exception(*args, **kwargs):
+            raise RuntimeError("test")
+
+        test_file = tmpdir.join("file.py")
+        test_file.write("import os")
+        assert io.File._open(str(test_file))
+
+        # correctly responds to error determining encoding
+        with patch("tokenize.detect_encoding", raise_arbitrary_exception):
+            with pytest.raises(Exception):
+                io.File._open(str(test_file))
