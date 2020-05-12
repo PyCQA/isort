f87fce3be0abd80bc5be898f83f66dc2a565cebc
Timothy Crosley <timothy.crosley@gmail.com>
2020-05-11 22:37:34 -0700

    Update dependencies

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,14 +8,14 @@ from isort.settings import Config
 
 
 def test_sort_file(tmpdir) -> None:
-    tmp_file = tmpdir.join(f"test_bad_syntax.py")
+    tmp_file = tmpdir.join("test_bad_syntax.py")
     tmp_file.write_text("""print('mismathing quotes")""", "utf8")
     with pytest.warns(UserWarning):
         api.sort_file(tmp_file, atomic=True)
     with pytest.warns(UserWarning):
         api.sort_file(tmp_file, atomic=True, write_to_stdout=True)
 
-    imperfect = tmpdir.join(f"test_needs_changes.py")
+    imperfect = tmpdir.join("test_needs_changes.py")
     imperfect.write_text("import b\nimport a\n", "utf8")
     api.sort_file(imperfect, write_to_stdout=True, show_diff=True)
 
@@ -29,11 +29,11 @@ def test_sort_file(tmpdir) -> None:
 
 
 def test_check_file(tmpdir) -> None:
-    perfect = tmpdir.join(f"test_no_changes.py")
+    perfect = tmpdir.join("test_no_changes.py")
     perfect.write_text("import a\nimport b\n", "utf8")
     assert api.check_file(perfect, show_diff=True)
 
-    imperfect = tmpdir.join(f"test_needs_changes.py")
+    imperfect = tmpdir.join("test_needs_changes.py")
     imperfect.write_text("import b\nimport a\n", "utf8")
     assert not api.check_file(imperfect, show_diff=True)
 
