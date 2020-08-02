eed66a9eb67da63e4d27aa7588ef5717f9dd3eff
Timothy Crosley <timothy.crosley@gmail.com>
2020-08-02 03:41:52 -0700

    Initial work toward adding experimental support for sorting literals #1358

---
+++ tests/test_literal.py
@@ -0,0 +1,33 @@
+import pytest
+
+import isort.literal
+from isort import exceptions
+from isort.settings import Config
+
+
+def test_value_mismatch():
+    with pytest.raises(exceptions.LiteralSortTypeMismatch):
+        isort.literal.assignment("x = [1, 2, 3]", "set", "py")
+
+
+def test_invalid_syntax():
+    with pytest.raises(exceptions.LiteralParsingFailure):
+        isort.literal.assignment("x = [1, 2, 3", "list", "py")
+
+
+def test_invalid_sort_type():
+    with pytest.raises(ValueError):
+        isort.literal.assignment("x = [1, 2, 3", "tuple-list-not-exist", "py")
+
+
+def test_invalid_sort_type():
+    with pytest.raises(ValueError):
+        isort.literal.assignment("x = [1, 2, 3", "tuple-list-not-exist", "py")
+
+
+def test_value_assignment():
+    assert isort.literal.assignment("x = ['b', 'a']", "list", "py") == "x = ['a', 'b']"
+    assert (
+        isort.literal.assignment("x = ['b', 'a']", "list", "py", Config(formatter="example"))
+        == 'x = ["a", "b"]'
+    )
