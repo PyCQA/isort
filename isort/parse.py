e2b2f8d0cab23dc32b9cae25de8e9d3f10b027aa
Timothy Crosley <timothy.crosley@gmail.com>
2020-09-03 02:31:21 -0700

    Resolve #1382: float to top

--- isort/parse.py
+++ isort/parse.py
@@ -200,6 +200,10 @@ def file_contents(contents: str, config: Config = DEFAULT_CONFIG) -> ParsedConte
         if skipping_line:
             out_lines.append(line)
             continue
+        elif config.float_to_top and import_index == -1 and line and not in_quote and not line.strip().startswith("#"):
+            import_index = index - 1
+            while import_index and not in_lines[import_index - 1]:
+                import_index -= 1
 
         line, *end_of_line_comment = line.split("#", 1)
         if ";" in line:
