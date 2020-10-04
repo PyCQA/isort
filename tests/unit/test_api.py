bdd1ebf0b50d8b0e8fc43a624db73966319c8ef6
Tamas Szabo <szabtam@gmail.com>
2020-10-05 01:37:40 +0300

    Fix test failures on Windows.

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,5 +1,6 @@
 """Tests the isort API module"""
 from io import StringIO
+import os
 from unittest.mock import MagicMock, patch
 
 import pytest
@@ -8,8 +9,8 @@ from isort import api
 from isort.settings import Config
 
 imperfect_content = "import b\nimport a\n"
-fixed_content = "import a\nimport b\n"
-fixed_diff = "\n+import a\n import b\n-import a\n"
+fixed_content = "import a\nimport b\n".replace('\n', os.linesep)
+fixed_diff = "\n+import a\n import b\n-import a\n".replace ('\n', os.linesep)
 
 
 @pytest.fixture
