092bdb15126710d285f160d84f1c883b3aee9b51
Timothy Crosley <timothy.crosley@gmail.com>
2020-10-07 23:35:49 -0700

    Implemented #1540: Officially support Python 3.9 stdlib imports by default.

--- isort/stdlibs/py3.py
+++ isort/stdlibs/py3.py
@@ -1,3 +1,3 @@
-from . import py35, py36, py37, py38
+from . import py35, py36, py37, py38, py39
 
-stdlib = py35.stdlib | py36.stdlib | py37.stdlib | py38.stdlib
+stdlib = py35.stdlib | py36.stdlib | py37.stdlib | py38.stdlib | py39.stdlib
