23897bf091e444c8556fcdec6f77db93653c2b3a
anirudnits <aniruddha97bhatt@gmail.com>
2020-10-10 20:29:52 +0530

    Added code to combine all bare straight imports in a single line when flag is set

--- isort/output.py
+++ isort/output.py
@@ -513,35 +513,71 @@ def _with_straight_imports(
     import_type: str,
 ) -> List[str]:
     output: List[str] = []
-    for module in straight_modules:
-        if module in remove_imports:
-            continue
 
-        import_definition = []
-        if module in parsed.as_map["straight"]:
-            if parsed.imports[section]["straight"][module]:
-                import_definition.append(f"{import_type} {module}")
-            import_definition.extend(
-                f"{import_type} {module} as {as_import}"
-                for as_import in parsed.as_map["straight"][module]
+    as_imports = any([module in parsed.as_map["straight"] for module in straight_modules])
+
+    # combine_straight_imports only works for bare imports, 'as' imports not included
+    if config.combine_straight_imports and not as_imports:
+        if not straight_modules:
+            return []
+
+        above_comments: List[str] = []
+        inline_comments: List[str] = []
+
+        for module in straight_modules:
+            if module in parsed.categorized_comments["above"]["straight"]:
+                above_comments.extend(parsed.categorized_comments["above"]["straight"].pop(module))
+
+        for module in parsed.categorized_comments["straight"]:
+            inline_comments.extend(parsed.categorized_comments["straight"][module])
+
+        combined_straight_imports = " ".join(straight_modules)
+        if inline_comments:
+            combined_inline_comments = " ".join(inline_comments)
+        else:
+            combined_inline_comments = ""
+
+        output.extend(above_comments)
+
+        if combined_inline_comments:
+            output.append(
+                f"{import_type} {combined_straight_imports}  # {combined_inline_comments}"
             )
         else:
-            import_definition.append(f"{import_type} {module}")
+            output.append(f"{import_type} {combined_straight_imports}")
 
-        comments_above = parsed.categorized_comments["above"]["straight"].pop(module, None)
-        if comments_above:
-            output.extend(comments_above)
-        output.extend(
-            with_comments(
-                parsed.categorized_comments["straight"].get(module),
-                idef,
-                removed=config.ignore_comments,
-                comment_prefix=config.comment_prefix,
+        return output
+
+    else:
+        for module in straight_modules:
+            if module in remove_imports:
+                continue
+
+            import_definition = []
+            if module in parsed.as_map["straight"]:
+                if parsed.imports[section]["straight"][module]:
+                    import_definition.append(f"{import_type} {module}")
+                import_definition.extend(
+                    f"{import_type} {module} as {as_import}"
+                    for as_import in parsed.as_map["straight"][module]
+                )
+            else:
+                import_definition.append(f"{import_type} {module}")
+
+            comments_above = parsed.categorized_comments["above"]["straight"].pop(module, None)
+            if comments_above:
+                output.extend(comments_above)
+            output.extend(
+                with_comments(
+                    parsed.categorized_comments["straight"].get(module),
+                    idef,
+                    removed=config.ignore_comments,
+                    comment_prefix=config.comment_prefix,
+                )
+                for idef in import_definition
             )
-            for idef in import_definition
-        )
 
-    return output
+        return output
 
 
 def _output_as_string(lines: List[str], line_separator: str) -> str:
