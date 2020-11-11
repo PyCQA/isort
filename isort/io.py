66ba8c6e43b723e6748e30149cfb640c0e42bd6f
Timothy Crosley <timothy.crosley@gmail.com>
2020-11-11 00:11:00 -0800

    black formatting

--- isort/io.py
+++ isort/io.py
@@ -29,9 +29,7 @@ class File:
     def from_contents(contents: str, filename: str) -> "File":
         encoding = File.detect_encoding(filename, BytesIO(contents.encode("utf-8")).readline)
         return File(  # type: ignore
-            stream=StringIO(contents),
-            path=Path(filename).resolve(),
-            encoding=encoding
+            stream=StringIO(contents), path=Path(filename).resolve(), encoding=encoding
         )
 
     @property
