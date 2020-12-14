71b058d6751d26bec36223970494da075df5a1b8
Timothy Crosley <timothy.crosley@gmail.com>
2020-12-13 18:24:50 -0800

    Initial work to pull out just import identification from parse

---
+++ isort/identify.py
@@ -0,0 +1,213 @@
+""""""
+from collections import OrderedDict, defaultdict
+from functools import partial
+from itertools import chain
+from typing import TYPE_CHECKING, Any, Dict, List, NamedTuple, Optional, Tuple
+from warnings import warn
+
+from . import place
+from .comments import parse as parse_comments
+from .deprecated.finders import FindersManager
+from .exceptions import MissingSection
+from .settings import DEFAULT_CONFIG, Config
+
+
+from isort.parse import _infer_line_separator, _normalize_line, _strip_syntax, skip_line
+
+
+def import_type(line: str, config: Config = DEFAULT_CONFIG) -> Optional[str]:
+    """If the current line is an import line it will return its type (from or straight)"""
+    if line.startswith(("import ", "cimport ")):
+        return "straight"
+    if line.startswith("from "):
+        return "from"
+    return None
+
+
+class ImportIdentified(NamedTuple):
+    from_file: Optional[Path]
+    line: int
+
+
+def imports(input_stream: TextIO, config: Config = DEFAULT_CONFIG) -> Iterator[ImportIdentified]:
+    """Parses a python file taking out and categorizing imports."""
+    in_quote = ""
+
+    indexed_input = enumerate(input_stream)
+    for index, line in indexed_input
+        statement_index = index
+        (skipping_line, in_quote) = skip_line(
+            line, in_quote=in_quote, index=index, section_comments=config.section_comments
+        )
+
+        if skipping_line:
+            continue
+
+        line, *end_of_line_comment = line.split("#", 1)
+        if ";" in line:
+            statements = [line.strip() for line in line.split(";")]
+        else:
+            statements = [line]
+        if end_of_line_comment:
+            statements[-1] = f"{statements[-1]}#{end_of_line_comment[0]}"
+
+        for statement in statements:
+            line, raw_line = _normalize_line(statement)
+            type_of_import = import_type(line, config) or ""
+            if not type_of_import:
+                out_lines.append(raw_line)
+                continue
+
+            import_string, _ = parse_comments(line)
+            line_parts = [part for part in _strip_syntax(import_string).strip().split(" ") if part]
+
+            if "(" in line.split("#", 1)[0]:
+                while not line.split("#")[0].strip().endswith(")"):
+                    try:
+                        index, next_line = next(indexed_input)
+                    except StopIteration:
+                        break
+
+                    line, _ = parse_comments(next_line)
+                    stripped_line = _strip_syntax(line).strip()
+                    import_string += line_separator + line
+            else:
+                while line.strip().endswith("\\"):
+                    index, next_line = next(indexed_input)
+                    line, _ = parse_comments(next_line)
+
+                    # Still need to check for parentheses after an escaped line
+                    if (
+                        "(" in line.split("#")[0]
+                        and ")" not in line.split("#")[0]
+                    ):
+                        stripped_line = _strip_syntax(line).strip()
+                        import_string += line_separator + line
+
+                        while not line.split("#")[0].strip().endswith(")"):
+                            try:
+                                index, next_line = next(indexed_input)
+                            except StopIteration:
+                                break
+                            line, _ = parse_comments(next_line)
+                            stripped_line = _strip_syntax(line).strip()
+                            import_string += line_separator + line
+
+                    stripped_line = _strip_syntax(line).strip()
+                    if import_string.strip().endswith(
+                        (" import", " cimport")
+                    ) or line.strip().startswith(("import ", "cimport ")):
+                        import_string += line_separator + line
+                    else:
+                        import_string = import_string.rstrip().rstrip("\\") + " " + line.lstrip()
+
+            if type_of_import == "from":
+                cimports: bool
+                import_string = (
+                    import_string.replace("import(", "import (")
+                    .replace("\\", " ")
+                    .replace("\n", " ")
+                )
+                if " cimport " in import_string:
+                    parts = import_string.split(" cimport ")
+                    cimports = True
+
+                else:
+                    parts = import_string.split(" import ")
+                    cimports = False
+
+                from_import = parts[0].split(" ")
+                import_string = (" cimport " if cimports else " import ").join(
+                    [from_import[0] + " " + "".join(from_import[1:])] + parts[1:]
+                )
+
+            just_imports = [
+                item.replace("{|", "{ ").replace("|}", " }")
+                for item in _strip_syntax(import_string).split()
+            ]
+
+            direct_imports = just_imports[1:]
+            straight_import = True
+            top_level_module = ""
+            if "as" in just_imports and (just_imports.index("as") + 1) < len(just_imports):
+                straight_import = False
+                while "as" in just_imports:
+                    nested_module = None
+                    as_index = just_imports.index("as")
+                    if type_of_import == "from":
+                        nested_module = just_imports[as_index - 1]
+                        top_level_module = just_imports[0]
+                        module = top_level_module + "." + nested_module
+                        as_name = just_imports[as_index + 1]
+                        direct_imports.remove(nested_module)
+                        direct_imports.remove(as_name)
+                        direct_imports.remove("as")
+                        if nested_module == as_name and config.remove_redundant_aliases:
+                            pass
+                        elif as_name not in as_map["from"][module]:
+                            as_map["from"][module].append(as_name)
+
+                        full_name = f"{nested_module} as {as_name}"
+                    else:
+                        module = just_imports[as_index - 1]
+                        as_name = just_imports[as_index + 1]
+                        if module == as_name and config.remove_redundant_aliases:
+                            pass
+                        elif as_name not in as_map["straight"][module]:
+                            as_map["straight"][module].append(as_name)
+
+                    del just_imports[as_index : as_index + 2]
+
+            if type_of_import == "from":
+                import_from = just_imports.pop(0)
+                placed_module = finder(import_from)
+                if config.verbose and not config.only_modified:
+                    print(f"from-type place_module for {import_from} returned {placed_module}")
+
+                elif config.verbose:
+                    verbose_output.append(
+                        f"from-type place_module for {import_from} returned {placed_module}"
+                    )
+                if placed_module == "":
+                    warn(
+                        f"could not place module {import_from} of line {line} --"
+                        " Do you need to define a default section?"
+                    )
+                root = imports[placed_module][type_of_import]  # type: ignore
+
+                if import_from not in root:
+                    root[import_from] = OrderedDict(
+                        (module, module in direct_imports) for module in just_imports
+                    )
+                else:
+                    root[import_from].update(
+                        (module, root[import_from].get(module, False) or module in direct_imports)
+                        for module in just_imports
+                    )
+
+            else:
+
+                for module in just_imports:
+
+                    placed_module = finder(module)
+                    if config.verbose and not config.only_modified:
+                        print(f"else-type place_module for {module} returned {placed_module}")
+
+                    elif config.verbose:
+                        verbose_output.append(
+                            f"else-type place_module for {module} returned {placed_module}"
+                        )
+                    if placed_module == "":
+                        warn(
+                            f"could not place module {module} of line {line} --"
+                            " Do you need to define a default section?"
+                        )
+                        imports.setdefault("", {"straight": OrderedDict(), "from": OrderedDict()})
+
+                    if placed_module and placed_module not in imports:
+                        raise MissingSection(import_module=module, section=placed_module)
+
+                    straight_import |= imports[placed_module][type_of_import].get(  # type: ignore
+                        module, False
+                    )
+                    imports[placed_module][type_of_import][module] = straight_import  # type: ignore
