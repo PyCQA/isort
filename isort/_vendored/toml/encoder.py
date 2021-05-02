6efb3acde018380ea1ef43b9e1d1f68c6b65eb02
keno <keno.ss57@gmail.com>
2021-05-03 01:41:22 +0900

    fix: Should import vendered version

--- isort/_vendored/toml/encoder.py
+++ isort/_vendored/toml/encoder.py
@@ -276,7 +276,7 @@ class TomlNumpyEncoder(TomlEncoder):
 
 class TomlPreserveCommentEncoder(TomlEncoder):
     def __init__(self, _dict=dict, preserve=False):
-        from toml.decoder import CommentValue
+        from .decoder import CommentValue
 
         super(TomlPreserveCommentEncoder, self).__init__(_dict, preserve)
         self.dump_funcs[CommentValue] = lambda v: v.dump(self.dump_value)
