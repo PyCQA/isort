f82ecc96e0a6bbebdea4431d99aed8c9855e94ae
Timothy Crosley <timothy.crosley@gmail.com>
2022-05-12 14:13:00 -0700

    Ignore some safety errors for numpy for pragmatic reasons

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,5 +7,5 @@ poetry run black --target-version py36 --check .
 poetry run isort --profile hug --check --diff isort/ tests/
 poetry run isort --profile hug --check --diff example_*/
 poetry run flake8 isort/ tests/
-poetry run safety check -i 39462 -i 40291
+poetry run safety check -i 39462 -i 40291 -i 43453 -i 44717 -i 44716 -i 44715 -i 47794
 poetry run bandit -r isort/ -x isort/_vendored
