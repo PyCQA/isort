f86106af791595cf8d67b2e26ec332de7a1d1418
clavedeluna <danalitovsky+git@gmail.com>
2022-12-03 08:41:51 -0300

    tests must install `example_isort_formatting_plugin`

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-./scripts/lint.sh
+cd example_isort_formatting_plugin && poetry install
+cd ..
 poetry run pytest tests/unit/ -s --cov=isort/ --cov-report=term-missing ${@-} --ignore=tests/unit/test_deprecated_finders.py
 poetry run coverage html
