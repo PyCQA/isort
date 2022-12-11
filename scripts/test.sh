5cb44e20c526ee9d4d5e3968d8c08a5b718ab89d
staticdev <staticdev-support@protonmail.com>
2022-12-11 20:39:26 +0100

    Improve test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-poetry run coverage run --parallel -m pytest tests/unit/ --ignore=tests/unit/test_deprecated_finders.py
-poetry run coverage html
+poetry run coverage run --parallel -m pytest tests/unit/ -s --ignore=tests/unit/test_deprecated_finders.py
+poetry run coverage combine
+poetry run coverage report
+poetry run coverage xml
