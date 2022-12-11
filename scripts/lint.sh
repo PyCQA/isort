db5460df7b8b1afeb90ac0a44f3110849c91bf3b
staticdev <staticdev-support@protonmail.com>
2022-12-11 22:25:18 +0100

    Prepare release

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-poetry run cruft check
+#poetry run cruft check
 poetry run mypy -p isort -p tests
 poetry run black --target-version py37 --check .
 poetry run isort --profile hug --check --diff isort/ tests/
