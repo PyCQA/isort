0ef29df739f2904431f7524e2059b9edcbece11a
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-12-04 22:01:32 +0200

    Add py312 to stdlibs

--- isort/stdlibs/py3.py
+++ isort/stdlibs/py3.py
@@ -1,3 +1,11 @@
-from . import py36, py37, py38, py39, py310, py311
+from . import py36, py37, py38, py39, py310, py311, py312
 
-stdlib = py36.stdlib | py37.stdlib | py38.stdlib | py39.stdlib | py310.stdlib | py311.stdlib
+stdlib = (
+    py36.stdlib
+    | py37.stdlib
+    | py38.stdlib
+    | py39.stdlib
+    | py310.stdlib
+    | py311.stdlib
+    | py312.stdlib
+)
--- tests/unit/test_importable.py
+++ tests/unit/test_importable.py
@@ -35,6 +35,7 @@ def test_importable():
     import isort.stdlibs.py39
     import isort.stdlibs.py310
     import isort.stdlibs.py311
+    import isort.stdlibs.py312
     import isort.utils
     import isort.wrap
     import isort.wrap_modes
