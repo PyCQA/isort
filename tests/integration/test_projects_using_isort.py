b96c0eabbcbe38aed9265383c54c44f775418985
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2025-01-20 08:52:20 +0100

    Add `xfail` for `django`

--- tests/integration/test_projects_using_isort.py
+++ tests/integration/test_projects_using_isort.py
@@ -13,6 +13,8 @@ from pathlib import Path
 from subprocess import check_call
 from typing import Generator, Sequence
 
+import pytest
+
 from isort.main import main
 
 
@@ -26,6 +28,10 @@ def run_isort(arguments: Generator[str, None, None] | Sequence[str]):
     main(["--check-only", "--diff", *arguments])
 
 
+@pytest.mark.xfail(
+    reason="Project is incorrectly formatted after PR #2236, should be fixed "
+    "after a release and the project formatting again."
+)
 def test_django(tmpdir):
     git_clone("https://github.com/django/django.git", tmpdir)
     run_isort(
