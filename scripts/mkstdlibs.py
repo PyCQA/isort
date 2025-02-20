5bc4793c9289fb9b32b06229663e5c8222f64bb3
devdanzin <74280297+devdanzin@users.noreply.github.com>
2025-02-20 07:19:55 -0300

    Fix mkstdlibs to use the stdlibs package, regenerate version files with correct module lists.

--- scripts/mkstdlibs.py
+++ scripts/mkstdlibs.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-import sys
-
-from stdlibs import py39
+from stdlibs import py39, py310, py311, py312, py313
 
 PATH = "isort/stdlibs/py{}.py"
-VERSIONS = [
-    ("3", "9"),
-    ("3", "10"),
-    ("3", "11"),
-    ("3", "12"),
-    ("3", "13"),
-]
+VERSIONS = {
+    ("3", "9"): py39,
+    ("3", "10"): py310,
+    ("3", "11"): py311,
+    ("3", "12"): py312,
+    ("3", "13"): py313,
+}
 
 DOCSTRING = """
 File contains the standard library of Python {}.
@@ -19,16 +17,13 @@ DO NOT EDIT. If the standard library changes, a new list should be created
 using the mkstdlibs.py script.
 """
 
-for version_info in VERSIONS:
+for version_info, version_module in VERSIONS.items():
     version = ".".join(version_info)
 
     # Any modules we want to enforce across Python versions stdlib can be included in set init
     modules = {"_ast", "posixpath", "ntpath", "sre_constants", "sre_parse", "sre_compile", "sre"}
 
-    if version_info == ("3", "9"):
-        modules |= py39.module_names
-    else:
-        modules |= sys.stdlib_module_names
+    modules |= version_module.module_names
     modules -= {"__future__", "__main__", "antigravity", "this"}
 
     path = PATH.format("".join(version_info))
