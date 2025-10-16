1fd010534eedffadec82289a08d449a5e17df1b8
Alex Liang <alex.liang@loftorbital.com>
2025-10-16 20:44:16 +0000

    chore: remove py3.8 types

--- isort/settings.py
+++ isort/settings.py
@@ -191,7 +191,7 @@ class _Config:
     force_sort_within_sections: bool = False
     lexicographical: bool = False
     group_by_package: bool = False
-    separate_packages: FrozenSet[str] = frozenset()
+    separate_packages: frozenset[str] = frozenset()
     ignore_whitespace: bool = False
     no_lines_before: frozenset[str] = frozenset()
     no_inline_sort: bool = False
