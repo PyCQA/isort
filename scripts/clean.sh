9a637804594d5d14355f32606c9e65e575a58ed7
joao.faria <joaovitorfaria.dev@gmail.com>
2025-11-06 23:42:56 -0300

    chore: replace black with ruff in clean.sh

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 
 uv run isort --profile hug isort/ tests/ scripts/
 uv run isort --profile hug example_*/
-uv run black isort/ tests/ scripts/
-uv run black example_*/
+uv run ruff format isort/ tests/ scripts/
+uv run ruff format example_*/
