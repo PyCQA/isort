5cff82a706fdcc632c54b2f07968bdcb8ffabf92
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2025-11-10 21:02:27 +0100

    Make config even more consistent

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,5 +3,4 @@ set -euxo pipefail
 
 uv run isort --profile hug isort/ tests/ scripts/
 uv run isort --profile hug example_*/
-uv run ruff format isort/ tests/ scripts/
-uv run ruff format example_*/
+uv run ruff format
