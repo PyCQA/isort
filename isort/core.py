ef581dff2e86d8268a7c48d30134a829fe251d5d
MUHAMED FAZAL PS <fazaleee123@gmail.com>
2026-06-29 18:36:36 +0530

    fix: sort_reexports should respect isort: skip comment
    When sort_reexports is enabled, __all__ assignments with
    # isort: skip were still being sorted. Added check for
    isort: skip comment before processing re-exports.
    
    Signed-off-by: Muhamed Fazal PS <fazaleee123cr7@gmail.com>

--- isort/core.py
+++ isort/core.py
@@ -244,7 +244,7 @@ def process(
                     code_sorting = stripped_line.split("isort: ")[1].strip()
                     code_sorting_indent = line[: -len(line.lstrip())]
                     not_imports = True
-                elif config.sort_reexports and stripped_line.startswith("__all__"):
+                elif config.sort_reexports and stripped_line.startswith("__all__") and "isort: skip" not in stripped_line:
                     _, rhs = stripped_line.split("=")
                     code_sorting = LITERAL_TYPE_MAPPING.get(rhs.lstrip()[0], "tuple")
                     code_sorting_indent = line[: -len(line.lstrip())]
