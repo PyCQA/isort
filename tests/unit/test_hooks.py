34bf5b5da0580e38039d49875447bbd7c7caa58f
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2026-08-11 21:31:10 +0200

    Split up git hook tests in preparation for compiling with `mypyc`

--- tests/unit/test_hooks.py
+++ tests/unit/test_hooks.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from subprocess import CompletedProcess
 from unittest.mock import MagicMock, patch
 
 from isort import exceptions, hooks
@@ -9,7 +10,9 @@ def test_git_hook(src_dir):
     """Simple smoke level testing of git hooks"""
 
     # Ensure correct subprocess command is called
-    with patch("subprocess.run", MagicMock()) as run_mock:
+    with patch(
+        "subprocess.run", return_value=CompletedProcess("command", returncode=0, stdout=b"")
+    ) as run_mock:
         hooks.git_hook()
         run_mock.assert_called_once()
         assert run_mock.call_args[0][0] == [
@@ -21,7 +24,9 @@ def test_git_hook(src_dir):
             "HEAD",
         ]
 
-    with patch("subprocess.run", MagicMock()) as run_mock:
+    with patch(
+        "subprocess.run", return_value=CompletedProcess("command", returncode=0, stdout=b"")
+    ) as run_mock:
         hooks.git_hook(lazy=True)
         run_mock.assert_called_once()
         assert run_mock.call_args[0][0] == [
@@ -34,25 +39,36 @@ def test_git_hook(src_dir):
 
     # Test that non python files aren't processed
     with patch(
-        "isort.hooks.get_lines",
-        MagicMock(return_value=["README.md", "setup.cfg", "LICDENSE", "mkdocs.yml", "test"]),
-    ):
-        with patch("subprocess.run", MagicMock()) as run_mock:
-            hooks.git_hook(modify=True)
-            run_mock.assert_not_called()
+        "subprocess.run",
+        return_value=CompletedProcess(
+            "command", returncode=0, stdout=b"README.md\nsetup.cfg\nLICDENSE\nmkdocs.yml\ntest"
+        ),
+    ) as run_mock:
+        hooks.git_hook(modify=True)
+        run_mock.assert_called_once()
 
+    # Test with incorrectly sorted file returned from git
+    file_name = os.path.join(src_dir, "main.py")
+    with patch(
+        "subprocess.run",
+        side_effect=[
+            CompletedProcess("command", returncode=0, stdout=file_name.encode()),
+            CompletedProcess("command", returncode=0, stdout=b"import b,a"),
+        ],
+    ) as run_mock:
+        errors = hooks.git_hook(modify=True, strict=True)
+        assert run_mock.call_count == 2
+        assert errors == 1
+
+
+def test_git_hook_with_mocks(src_dir: str) -> None:
+    """
+    Additional tests for the git hook, split off to allow running some when compiled.
+    """
     mock_main_py = MagicMock(return_value=[os.path.join(src_dir, "main.py")])
 
     mock_imperfect = MagicMock()
     mock_imperfect.return_value.stdout = b"import b\nimport a"
-
-    # Test with incorrectly sorted file returned from git
-    with patch("isort.hooks.get_lines", mock_main_py):
-        with patch("subprocess.run", mock_imperfect):
-            with patch("isort.api.sort_file", MagicMock(return_value=False)) as api_mock:
-                hooks.git_hook(modify=True)
-                api_mock.assert_called_once()
-                assert api_mock.call_args[0][0] == mock_main_py.return_value[0]
 
     # Test with sorted file returned from git and modify=False
     with patch("isort.hooks.get_lines", mock_main_py):
@@ -62,14 +78,12 @@ def test_git_hook(src_dir):
                 api_mock.assert_not_called()
 
     # Test with skipped file returned from git
-    with patch(
-        "isort.hooks.get_lines", MagicMock(return_value=[os.path.join(src_dir, "main.py")])
-    ) as run_mock:
+    with patch("isort.hooks.get_lines", MagicMock(return_value=[os.path.join(src_dir, "main.py")])):
 
         class FakeProcessResponse:
             stdout = b"# isort: skip-file\nimport b\nimport a\n"
 
-        with patch("subprocess.run", MagicMock(return_value=FakeProcessResponse())) as run_mock:
+        with patch("subprocess.run", MagicMock(return_value=FakeProcessResponse())):
             with patch("isort.api", MagicMock(side_effect=exceptions.FileSkipped("", ""))):
                 hooks.git_hook(modify=True)
 
@@ -80,13 +94,18 @@ def test_git_hook_lazy(tmpdir):
     has_problems = tmpdir.join("test_has_problems.py")
     has_problems.write_text("import b\nimport a\n", "utf8")
 
-    # get_lines is called by git_hook to get the list of modified files to be checked
-    with patch("isort.hooks.get_lines", MagicMock(return_value=[str(has_problems)])):
-        with patch("subprocess.run", MagicMock(side_effect=RuntimeError("donotcall"))) as run_mock:
-            found_errors = hooks.git_hook(lazy=True, strict=True)
-            # subprocess.run (via get_output) should not be called when lazy=True
-            # because we're not asking git for the staged content
-            assert run_mock.call_count == 0
+    with patch(
+        "subprocess.run",
+        side_effect=[
+            CompletedProcess("command", returncode=0, stdout=str(has_problems).encode()),
+            # Second call to subprocess.run should not be made when lazy=True
+            RuntimeError("donotcall"),
+        ],
+    ) as run_mock:
+        found_errors = hooks.git_hook(lazy=True, strict=True)
+        # subprocess.run (via get_output) should not be called when lazy=True
+        # because we're not asking git for the staged content
+        assert run_mock.call_count == 1
 
     assert found_errors == 1
 
